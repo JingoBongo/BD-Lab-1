4e022bf0800b4774450905db0d2ae75d333552c9
JingoBongo <43173309+JingoBongo@users.noreply.github.com>
2018-12-06 09:27:39 +0200

    Add files via upload

---
+++ BD-lab-7/README.sql
@@ -0,0 +1,23 @@
+### 1
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex1.PNG)
+
+### 2
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex2.PNG)
+
+### 3
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex3.PNG)
+
+### 4
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex4.PNG)
+
+### 5
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex1v2after5.PNG)
+
+### 6
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex6.PNG)
+
+### 7
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex7.PNG)
+
+### 8
+![Alt text](https://github.com/JingoBongo/BD-Lab-2-in-documents-/blob/master/BD-lab-7/ex8.PNG)
